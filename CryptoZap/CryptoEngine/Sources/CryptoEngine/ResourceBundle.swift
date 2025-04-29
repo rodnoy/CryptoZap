915afa901c0cefd23c66286be9ff86d10a18dc08
Kirill Simagin <ksimagin@gmail.com>
2025-04-29 09:25:57 +0200

    updated bundle path

--- CryptoZap/CryptoEngine/Sources/CryptoEngine/ResourceBundle.swift
+++ CryptoZap/CryptoEngine/Sources/CryptoEngine/ResourceBundle.swift
@@ -12,18 +12,25 @@ public enum ResourceBundle {
     public static let bundle: Bundle = {
         let fm = FileManager.default
 
-        // Homebrew installation path (include Cellar/cryptozap-cli/<version>)
-        let cellarPath = "/opt/homebrew/Cellar/cryptozap-cli"
-        if let versions = try? fm.contentsOfDirectory(atPath: cellarPath).sorted(by: >) {
-            for version in versions {
-                let bundlePath = "\(cellarPath)/\(version)/CryptoEngine_CryptoEngine.bundle"
-                if fm.fileExists(atPath: bundlePath), let bundle = Bundle(path: bundlePath) {
-                    return bundle
+        // Universal detection for Homebrew Cellar path (arm64 or x86_64)
+        let possibleCellarPaths = [
+            "/opt/homebrew/Cellar",     // Apple Silicon
+            "/usr/local/Cellar"         // Intel
+        ]
+
+        for cellarRoot in possibleCellarPaths {
+            let cryptozapPath = "\(cellarRoot)/cryptozap-cli"
+            if let versions = try? fm.contentsOfDirectory(atPath: cryptozapPath).sorted(by: >) {
+                for version in versions {
+                    let bundlePath = "\(cryptozapPath)/\(version)/CryptoEngine_CryptoEngine.bundle"
+                    if fm.fileExists(atPath: bundlePath), let bundle = Bundle(path: bundlePath) {
+                        return bundle
+                    }
                 }
             }
         }
 
-        // local launch: with binary
+        // Local execution: next to binary
         let localPath = Bundle.main.bundleURL.deletingLastPathComponent()
             .appendingPathComponent("CryptoEngine_CryptoEngine.bundle").path
         if fm.fileExists(atPath: localPath), let bundle = Bundle(path: localPath) {
